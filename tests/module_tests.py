30e6b019e697ab3c86e93f546259f3a79c6fa64b
chusov.e <chusoveg17@gmail.com>
2024-12-25 23:03:52 +0500

    Fix tests path for pytests running

--- tests/module_tests.py
+++ tests/module_tests.py
@@ -9,7 +9,7 @@ from UNET.model_common_functions import unet_full_model
 
 def get_model():
     model = unet_full_model()
-    model.load_weights('../UNET/best_model.h5')
+    model.load_weights('./UNET/best_model.h5')
     return model
 
 
@@ -37,7 +37,7 @@ def test_should_correctly_get_parameters_from_filename(filename, expected_D, exp
     assert tp == expected_tp
 
 def test_predict_parameters_success():
-    image = Image.open('./tests_files/L32_D0.0821_V0.3536_tb0.7561_tp0.5453_tn0.0000_tpn0.0000_J0.2500_results.jpg')
+    image = Image.open('./tests/tests_files/L32_D0.0821_V0.3536_tb0.7561_tp0.5453_tn0.0000_tpn0.0000_J0.2500_results.jpg')
     image = image.resize((128, 128))
     np_image = img_to_array(image, data_format = "channels_first") / 255.0
     model = get_model()
